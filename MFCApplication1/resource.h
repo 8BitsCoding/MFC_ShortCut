344c83a2d935d0b6889b12ac94ea439670398416
taehyung77@naver.com <taehyung77@naver.com>
2019-08-17 08:59:33 +0900

    commit

---
+++ MFCApplication1/resource.h
@@ -0,0 +1,20 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// MFCApplication1.rc에서 사용되고 있습니다.
+//
+#define IDD_MFCAPPLICATION1_DIALOG      102
+#define IDR_MAINFRAME                   128
+#define IDR_MY_ACC                      130
+#define ID_EDIT_CLEAR_CMD               32771
+#define ID_START_CMD                    32772
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32774
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
